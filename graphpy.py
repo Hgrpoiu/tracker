d13d9e087b5e1d0fb7c6c5bce5db520344ec7a58
Hgrpoiu <hgrpoiu@gmail.com>
2022-06-08 17:42:17 -0500

    A push which brings in tkinter.

---
+++ graphpy.py
@@ -0,0 +1,4 @@
+#This file is meant to handle data sets and graph them accordingly.
+#First, I will implement an interpreator class meant to be tailored to different data types.
+#Then, from this interpreator there will be a png creator
+
